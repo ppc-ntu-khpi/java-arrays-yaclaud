b9a8c584459d40bc3663492deefdc9974b320f74
claud <56557403+yaclaud@users.noreply.github.com>
2023-02-14 22:57:20 +0200

    Add files via upload

--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -6,7 +6,7 @@ public class TestResult {
 
     public static void main(String[] args) {
 
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+        int size = 11;
+        System.out.println(Exercise.Calculate(size));
     }
 }
